f4d40b8a303dc392a938afae8acf8eb267499a4d
JEWEL <163345491+AnalyticalJewel@users.noreply.github.com>
2025-06-22 23:45:27 +0530

    Updated the index code for frontend and also modified the import flask by adding render_template

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pickle
 import pandas as pd 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify,render_template
 
 with open("model.pkl", "rb") as f:
     model = pickle.load(f)
@@ -9,18 +9,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Love Score Prediction API is Live"
+    return render_template("index.html")  # Serve the frontend
 
 @app.route("/predict", methods=["POST"])
 def predict():
     data = request.get_json()
-
-    try:
-        df_input = pd.DataFrame([data])
-        prediction = model.predict(df_input)[0]
-        return jsonify({"predicted_love_score": round(prediction, 2)})
-    except Exception as e:
-        return jsonify({"error": str(e)}), 400
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    df_input = pd.DataFrame([data])
+    prediction = model.predict(df_input)[0]
+    return jsonify({"predicted_love_score": round(prediction, 2)})
